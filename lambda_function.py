8cbc824540aec75e3a6b7d54ec01be11f125ba19
Gregory Eberwine <geberwine@gmail.com>
2026-03-27 16:49:41 -0400

    added Lambda function

---
+++ lambda_function.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+
+client = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    get = client.get_item(
+        TableName='websiteTable',
+        Key={
+            'counter': {
+                'S':'visitors'
+            }
+        },
+        ProjectionExpression='numberVisitors'
+    )
+
+    increment = int(get['Item']['numberVisitors']['N']) + 1
+
+    put = client.put_item(
+        TableName='websiteTable',
+        Item={
+            'counter': {'S': 'visitors'},
+            'numberVisitors': {'N': str(increment)}
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'numberVisitors': increment})
+    }
